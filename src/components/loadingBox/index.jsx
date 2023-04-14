d1aa2ee136a2d3945e3e9f0660732a5d07dc70e2
zhouwei <weech0u@foxmail.com>
2023-04-14 20:24:57 +0800

    feat: 添加loading状态

---
+++ src/components/loadingBox/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './loadingBox.less'
+
+export default function LoadingBox (props) {
+  return (
+    <div id='loadingBox' style={props.styles}>
+      <div id='pa'>─=≡Σ(((つ•̀ω•́)つ</div>
+    </div>
+  )
+}
